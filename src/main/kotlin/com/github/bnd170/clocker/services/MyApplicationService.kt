a450d7f78477ac57d7a9e74ca28c1e791528940c
GitHub Action <action@github.com>
2021-12-02 16:28:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/bnd170/clocker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bnd170/clocker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bnd170.clocker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bnd170.clocker.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bnd170/clocker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bnd170/clocker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bnd170.clocker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bnd170.clocker.MyBundle
 
 class MyProjectService(project: Project) {
 
